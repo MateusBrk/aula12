29927c5cee449ec0d79bff5fb9cb28dff04b70c8
MateusBrk <161601930+MateusBrk@users.noreply.github.com>
2024-05-22 12:25:48 -0300

    Add files via upload

---
+++ aula12/src/Eevee.java
@@ -0,0 +1,20 @@
+public class Eevee {
+   private String tipo;
+   private int altura, HP;
+   private double peso;
+
+public String ataque(){
+    return "Tackle";
+}
+
+public String defesa(){
+    return "Growl";
+}
+
+public String especial(){
+    return "Bite";
+}
+    
+
+
+}
---
+++ aula12/src/Jolteon.java
@@ -0,0 +1,15 @@
+public class Jolteon extends Eevee 
+{
+    @Override
+    public String ataque(){
+        return "ThunderShock";
+    }
+    @Override
+    public String defesa(){
+        return "Charge";
+    }
+    @Override
+    public String especial(){
+        return "Thunder";
+    }
+}
